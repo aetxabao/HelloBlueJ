c12ce8d43b19fd51d6ac386049c1ef415754d7ae
aetxabao <aitor.etxabarren@gmail.com>
2021-08-09 01:04:56 +0200

    Initial sharing of project

---
+++ HelloBlueJ.java
@@ -0,0 +1,18 @@
+
+/**
+ * Clase para imprimir un saludo de inicio.
+ *
+ * @author aetxabao
+ * @version 0.01
+ */
+public class HelloBlueJ
+{
+    /**
+     * Método inicial
+     */
+    public static void main(String[] args)
+    {
+        // Imprime una línea en el terminal 
+        System.out.println("Hello world!");
+    }
+}
